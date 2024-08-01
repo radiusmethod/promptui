e0342c36e10f0857140ba8749279a49e55dd09ac
Patrick Joyce <pjaudiomv@gmail.com>
2024-08-01 00:57:59 -0400

    add example

---
+++ _examples/basic_text/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/radiusmethod/promptui"
+)
+
+func main() {
+	items := []string{"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"}
+
+	prompt := promptui.Select{
+		Label:             "Select an Option",
+		Items:             items,
+		Size:              3,
+		StartInSearchMode: true,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You chose %q\n", result)
+}
